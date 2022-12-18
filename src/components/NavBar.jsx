6037fc1ba3c649cc5cf50a9e8e3c9c1b067ee10f
fachmi firmansyah <fachmi.firmansyah@evos.gg>
2022-12-18 13:23:29 +0700

    feat(genre and search)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,34 +1,97 @@
-import React, { Fragment, useState} from 'react'
-import {HiSearch} from 'react-icons/hi'
-import '../Styles/NavBarStyle.css'
-import {Routes, Route, NavLink} from 'react-router-dom'
-import Movies from './Movies'
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
+import { Routes, Route, NavLink } from "react-router-dom";
+import "../Styles/NavBarStyle.css";
 
-export const Container = React.createContext()
+import { HiSearch } from "react-icons/hi";
+import Movies from "./Movies";
+
+import { NavDropdown } from "react-bootstrap";
+
+export const Container = React.createContext();
+
+const genre = ["Action", "Adventure", "Comedy", "Drama", "Horror"];
 
 function Navbar() {
-    const [toggle, setToggle] = useState(true)
+  const [toggle, setToggle] = useState(true);
+  const [moviesData, setMoviesData] = useState([]);
+  const [genresData, setGenresData] = useState([]);
+
+  const Api = "https://api.themoviedb.org/3";
+
+  const SearchMovies = async (search) => {
+    const data = await axios.get(Api + "/search/movie", {
+      params: {
+        api_key: "ba8f4caa4e6ebec49f8c6b8ba603ed04",
+        query: search,
+      },
+      
+    });
+    const results = data.data.results;
+    setMoviesData(results);
+  };
+  const getGenres = async () => {
+    const data = await axios.get(Api + "/genre/movie/list", {
+      params: {
+        api_key: "ba8f4caa4e6ebec49f8c6b8ba603ed04",
+      },
+      
+    });
+    const results = data.data.genres;
+    setGenresData(results);
+  };
+
+  const MovieCall = async () => {
+    const data = await axios.get(Api + "/discover/movie", {
+      params: {
+        api_key: "ba8f4caa4e6ebec49f8c6b8ba603ed04",
+      },
+    });
+    const results = data.data.results;
+    setMoviesData(results);
+  };
+
+  useEffect(() => {
+    MovieCall();
+    getGenres();
+  }, []);
+
   return (
     <Container.Provider value={toggle}>
-    <Fragment>
-        <nav className="navBarColor">
-            <div className='nav-options'>
-                <h1 id='heading'>7Flix</h1>
-                <NavLink to=''>
-                <span id='MoviesLight'>Movies</span>
-                </NavLink>
-            </div>
-            <div className='input-group'>
-            <input type="text" placeholder='Search movies'></input>
-            <HiSearch fontSize={21} color='grey' id='search'/>
-            </div>
+      <Fragment>
+        <nav className="navBarColor nav-bar">
+          <div className="nav-options">
+            <h1 id="heading">7Flix</h1>
+            <NavLink to="">
+              <span id="MoviesLight">Movies</span>
+            </NavLink>
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title="KATEGORI"
+              menuVariant="dark"
+            >
+              {genresData.map((item) => (
+                <div onClick={() => SearchMovies(item.name)}>
+                    <NavDropdown.Item>{item.name}</NavDropdown.Item>
+                </div>
+              ))}
+            </NavDropdown>
+          </div>
+          <div className="input-grouped">
+            <input
+              type="text"
+              placeholder="Search movies"
+              onChange={(e) => SearchMovies(e.target.value)}
+            ></input>
+            <HiSearch fontSize={21} color="grey" id="search" />
+          </div>
         </nav>
         <Routes>
-            <Route path='' element={<Movies/>}/>
+          <Route path="" element={<Movies data={moviesData} />} />
         </Routes>
-    </Fragment>
+      </Fragment>
     </Container.Provider>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
