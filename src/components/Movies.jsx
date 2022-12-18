b8496e000fcff9174a1ae2a456046c5d77d7665e
Savirsaz8 <savira.aliva@gmail.com>
2022-12-18 23:09:09 +0700

    add video modal

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,12 +3,12 @@ import React, {Fragment} from 'react'
 import {AiFillPlayCircle} from 'react-icons/ai'
 import NoImg from './NoImg.png'
 import '../Styles/Videos.css'
+import Player from "./Player";
 
 function Movies({data}) {
     // const {toggle} = useContext(Container)
    
-    const Images = "https://image.tmdb.org/t/p/w500"
-
+    const Images = "https://image.tmdb.org/t/p/w500";
 
   return (
     <Fragment>
@@ -17,11 +17,12 @@ function Movies({data}) {
         {data.map((movie) => {
             return(
             <Fragment>
-                <div id='container' className='card-movies'>
-                    <AiFillPlayCircle color='red' fontSize={40} id='playIcon'/>
-                    <img src={movie.poster_path ? `${Images}${movie.poster_path}` : NoImg } alt=''/>
-                    <h3>{movie.title}</h3>
-                </div>
+                <Player media_type="movie" id={movie.id} movie_data={movie}>
+                    <div id={movie.id} className='card-movies container'>
+                        <img src={movie.poster_path ? `${Images}${movie.poster_path}` : NoImg } alt=''/>
+                        <h3>{movie.title}</h3>
+                    </div>
+                </Player>
             </Fragment>
             )
         })}
